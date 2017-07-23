61855626990cbc916f02099e42ec9b1fdbd6bc2c
p4telj <pateljim@umich.edu>
2017-07-23 09:38:39 -0400

    determined static file directories

--- LibShelf/settings.py
+++ LibShelf/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'LibShelf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(__file__), '../templates').replace('\\', '/'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
@@ -117,4 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join('/static/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticGlobal'),
+    os.path.join(BASE_DIR, 'LibShelf', 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticDeploy')
--- LibShelf/urls.py
+++ LibShelf/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from LibShelf.views import home_page
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', home_page)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
